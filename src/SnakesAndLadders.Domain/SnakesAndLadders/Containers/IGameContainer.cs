34e671a174ac2548151630490912090b4e28d3b3
skucherenko <kucherenkoserhiy@gmail.com>
2021-12-28 22:15:31 +0100

    Added game container; updated class diagram

---
+++ src/SnakesAndLadders.Domain/SnakesAndLadders/Containers/IGameContainer.cs
@@ -0,0 +1,9 @@
+﻿using SnakesAndLadders.Domain.SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Domain.SnakesAndLadders.Containers
+{
+    public interface IGameContainer
+    {
+        public Game Game { get; set; }
+    }
+}
