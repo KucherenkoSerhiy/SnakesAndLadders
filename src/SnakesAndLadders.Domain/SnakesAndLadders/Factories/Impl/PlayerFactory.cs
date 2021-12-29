bc07cdeaf4f2771327a124186d411e0fe867362a
skucherenko <kucherenkoserhiy@gmail.com>
2021-12-29 02:13:35 +0100

    tested and implemented player factory

---
+++ src/SnakesAndLadders.Domain/SnakesAndLadders/Factories/Impl/PlayerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using SnakesAndLadders.Domain.SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Domain.SnakesAndLadders.Factories.Impl
+{
+    public class PlayerFactory: IPlayerFactory
+    {
+        public List<PlayerToken> Build(int numberOfPlayers)
+        {
+            if (numberOfPlayers <= 0)
+                throw new ArgumentException(nameof(numberOfPlayers));
+            
+            var players = new List<PlayerToken>();
+
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                var player = new PlayerToken
+                {
+                    Name = $"Player {i}",
+                    Position = 1,
+                    IsTheirTurn = false
+                };
+                players.Add(player);
+            }
+
+            players[0].IsTheirTurn = true;
+
+            return players;
+        }
+    }
+}
---
+++ test/unit/SnakesAndLadders.Domain.UnitTest/SnakesAndLadders/Factories/PlayerFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using SnakesAndLadders.Domain.SnakesAndLadders.Factories.Impl;
+using SnakesAndLadders.Domain.SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Domain.UnitTest.SnakesAndLadders.Factories
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class PlayerFactoryTests
+    {
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void Build_InvalidNumberOfPlayers_ThrowsArgumentException(int numberOfPlayers)
+        {
+            var sut = new PlayerFactory();
+            Assert.Throws<ArgumentException>(() => sut.Build(numberOfPlayers));
+        }
+        
+        [Test]
+        public void Build_OnePlayer_Ok()
+        {
+            var expectedPlayers = new List<PlayerToken>
+            {
+                new PlayerToken
+                {
+                    Name = "Player 1",
+                    Position = 1,
+                    IsTheirTurn = true
+                }
+            };
+            var sut = new PlayerFactory();
+            var players = sut.Build(1);
+            players.Should().BeEquivalentTo(expectedPlayers);
+        }
+        
+        [Test]
+        public void Build_TwoPlayers_Ok()
+        {
+            var expectedPlayers = new List<PlayerToken>
+            {
+                new()
+                {
+                    Name = "Player 1",
+                    Position = 1,
+                    IsTheirTurn = true
+                },
+                new()
+                {
+                    Name = "Player 2",
+                    Position = 1,
+                    IsTheirTurn = false
+                }
+            };
+            var sut = new PlayerFactory();
+            var players = sut.Build(2);
+            players.Should().BeEquivalentTo(expectedPlayers);
+        }
+    }
+}
