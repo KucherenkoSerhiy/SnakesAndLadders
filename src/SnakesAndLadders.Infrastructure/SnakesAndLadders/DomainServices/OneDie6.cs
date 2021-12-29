d9153c742803810fc9e0327ad4ca28bff4f4d444
skucherenko <kucherenkoserhiy@gmail.com>
2021-12-29 02:04:11 +0100

    tested and implemented Die

---
+++ src/SnakesAndLadders.Infrastructure/SnakesAndLadders/DomainServices/OneDie6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SnakesAndLadders.Domain.SnakesAndLadders.Services;
+
+namespace SnakesAndLadders.Infrastructure.SnakesAndLadders.DomainServices
+{
+    public class OneDie6: IDie
+    {
+        private const int MinValue = 1;
+        private const int MaxValue = 6;
+        private readonly Random _rng;
+
+        public OneDie6()
+        {
+            _rng = new Random();
+        }
+        
+        public int Roll()
+        {
+            var roll = _rng.Next(MinValue, MaxValue + 1);
+            return roll;
+        }
+    }
+}
---
+++ test/unit/SnakesAndLadders.Infrastructure.UnitTest/SnakesAndLadders/DomainServices/OneDie6Tests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using SnakesAndLadders.Infrastructure.SnakesAndLadders.DomainServices;
+
+namespace SnakesAndLadders.Infrastructure.UnitTest.SnakesAndLadders.DomainServices
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class OneDie6Tests
+    {
+        [Test]
+        public void Roll_10000Times_ReturnsValuesBetween1And6()
+        {
+            var totalRolls = 10000;
+            var sut = new OneDie6();
+            var rollCounter = new Dictionary<int, int>();
+
+            for (var i = 0; i < totalRolls; i++)
+            {
+                var roll = sut.Roll();
+                if (!rollCounter.ContainsKey(roll))
+                    rollCounter[roll] = 1;
+                else
+                    rollCounter[roll]++;
+            }
+
+            var validRolls = 0;
+            for (var i = 1; i <= 6; i++)
+                validRolls += rollCounter[i];
+            validRolls.Should().Be(totalRolls);
+
+            for (var i = 1; i <= 6; i++)
+                rollCounter[i].Should().BeGreaterThan(0);
+        }
+    }
+}
