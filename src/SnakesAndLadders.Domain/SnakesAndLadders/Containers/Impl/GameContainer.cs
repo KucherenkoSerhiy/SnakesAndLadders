ab943e16717fd3896e144df0827327f6e8dd8ad2
skucherenko <kucherenkoserhiy@gmail.com>
2021-12-29 02:14:52 +0100

    added GameContainer implementation

---
+++ src/SnakesAndLadders.Domain/SnakesAndLadders/Containers/Impl/GameContainer.cs
@@ -0,0 +1,9 @@
+﻿using SnakesAndLadders.Domain.SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Domain.SnakesAndLadders.Containers.Impl
+{
+    public class GameContainer : IGameContainer
+    {
+        public Game Game { get; set; }
+    }
+}
